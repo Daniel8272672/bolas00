66b82ea37704978f4b222dd360c320b42edffc72
Daniel8272672 <101835910+Daniel8272672@users.noreply.github.com>
2025-03-14 09:07:41 -0300

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,4 +1,16 @@
-document.getElementById('btnquery').addEventListener('click', function() {
+document.getElementById('btnquery').addEventListener('click',function() {
     let circulo = document.querySelector(".circulos .circulo");
     circulo.classList.toggle("ativo");
+
+});
+
+
+document.getElementById("btnqueryAll").addEventListener("click", function () {
+    let todosCirculos = document.querySelectorAll(".circulo");
+    todosAtivos = true;
+    todosAtivos = Array.from(todosCirculos).every(el => el.classList.contains('ativo'));
+
+    todosCirculos.forEach(circ => {
+        circ.classList.toggle("ativo", !todosAtivos);
+    });
 });
